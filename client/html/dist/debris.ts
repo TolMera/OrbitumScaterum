8d541e077924ff5c8e5335b137396611a746f8c7
TolMera <bjorn.macintosh@gmail.com>
2023-09-30 23:52:13 +1000

    I have real orbital mechanics WOOHOO!

--- client/html/dist/debris.ts
+++ client/html/dist/debris.ts
@@ -13,7 +13,7 @@ type Vector = Point;
 const G = 6.67430e-11;
 const M = 5.972e24;
 
-type DebrisRecord = {
+export type DebrisRecord = {
     point: Point,
     vector: Vector,
     speed: number,
@@ -21,10 +21,8 @@ type DebrisRecord = {
 };
 
 export class DebrisModel {
-    records: DebrisRecord[] = [];
-
-    minimumOrbit = 13_000;
-    maximumOrbit = 750_000;
+    minimumOrbit = 1_000;
+    maximumOrbit = 7_250;
     
     newDebris() {
         const point: Point = {
@@ -35,18 +33,18 @@ export class DebrisModel {
             x: 0,
             y: Math.sqrt(G * M / Math.abs(point.x))
         };
-        const speed: number = G * M / point.x;
-        const update = function (item, time) {
-          const r = Math.sqrt(item.point.x * item.point.x + item.point.y * item.point.y);
+        const speed: number = G * M / (point.x - 7680);
+        const update = function (time: number) {
+          const r = Math.sqrt(point.x * point.x + point.y * point.y);
           const a = G * M / (r * r);
           
           // Update velocity using acceleration
-          item.vector.x += (-a * item.point.x / r) * time;
-          item.vector.y += (-a * item.point.y / r) * time;
+          vector.x += (-a * point.x / r) * time;
+          vector.y += (-a * point.y / r) * time;
           
           // Update position using velocity
-          item.point.x += item.vector.x * time;
-          item.point.y += item.vector.y * time;
+          point.x += vector.x * time;
+          point.y += vector.y * time;
         }
         
         const record = {
@@ -56,7 +54,6 @@ export class DebrisModel {
             update
         };
 
-        this.records.push(record);
         return record;
     }
 }
